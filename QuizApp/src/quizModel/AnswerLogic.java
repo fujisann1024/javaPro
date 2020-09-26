fce265dcc4efdc4270e7d4305b7d915c1a24992f
fujisann1024 <tehepero302@gmail.com>
2020-09-26 18:50:14 +0900

    idea

---
+++ QuizApp/src/quizModel/AnswerLogic.java
@@ -0,0 +1,20 @@
+package quizModel;
+
+import java.util.List;
+
+public class AnswerLogic {
+	//第一引数～インスタンス
+	//第二引数～List型の配列
+	public void answerLog(AnswerData answer,
+	//AnswerDataインスタンスが格納できるList(ArrayList)配列
+	List<AnswerData> answerDataList) {
+		/*addメソッド
+		第一引数～指定のインデックス番号
+		第二引数～値(インスタンス)
+		指定の位置に値がある場合は上書きするのではなく
+		指定の位置に値を格納し,もともとあった値を後ろ
+		にずらす
+		*/
+		answerDataList.add(0,answer);
+	}
+}
